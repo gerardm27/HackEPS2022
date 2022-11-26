fc5605a3c6ddb0dd3616a89b1d70feba1c6483e0
AwaKT <gerardm27@gmail.com>
2022-11-27 00:00:39 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ client/gsf/components/Mapa.js
@@ -0,0 +1,37 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+const Mapa = () => {
+  return (
+    <MapContainer 
+      //Options for the map
+      attributionControl={false} 
+      center={[51.505, -0.09]} 
+      zoom={13} 
+      scrollWheelZoom={true} 
+      boxZoom={true}
+      touchZoom={true}
+      style={{height: 500, width: "100%"}}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker 
+        position={[51.505, -0.09]}>
+        
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+      <Marker 
+        position={[51.52, -0.2]}>
+        
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  )
+}
+
+export default Mapa
